154e312a8f89c95187f1efc5f11a1fbfe762c9f1
Vinicius Vieira <viniciusmv91@gmail.com>
2026-02-16 12:25:53 -0300

    Add DeleteProductByIdCommand and handler with MediatR
    Introduced DeleteProductByIdCommand and its handler in the Catalog.Application.Commands namespace. The handler uses IProductRepository to delete a product by ID and returns a boolean result, leveraging MediatR for request handling.

---
+++ Services/Catalog/Catalog.Application/Commands/DeleteProductByIdCommand.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Commands;
+
+public record DeleteProductByIdCommand(string Id) : IRequest<bool>
+{
+}
+
+public record DeleteProductByIdCommandHandler : IRequestHandler<DeleteProductByIdCommand, bool>
+{
+    private readonly IProductRepository _productRepository;
+
+    public DeleteProductByIdCommandHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<bool> Handle(DeleteProductByIdCommand request, CancellationToken cancellationToken)
+    {
+        return await _productRepository.DeleteProduct(request.Id);
+    }
+}

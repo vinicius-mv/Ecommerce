fd9ee5f26e1f147eda184fe8aaaf7a54e5ef3fac
Vinicius Vieira <viniciusmv91@gmail.com>
2026-02-16 19:49:49 -0300

    Add MongoDB BrandRepository and config settings
    Added MongoDB connection and collection settings to appsettings.json. Introduced BrandRepository for ProductBrand management using MongoDB and configuration. Added Microsoft.Extensions.Configuration package. Removed placeholder Class1.cs file.

---
+++ Services/Catalog/Catalog.Infrastructure/Repositories/BrandRepository.cs
@@ -0,0 +1,29 @@
+﻿using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.Infrastructure.Repositories;
+
+public class BrandRepository : IBrandRepository
+{
+    private readonly IMongoCollection<ProductBrand> _brands;
+
+    public BrandRepository(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration["DatabaseSettings:ConnectionString"]);
+
+        var catalogDb = client.GetDatabase(configuration["DatabaseSettings:DatabaseName"]);
+        _brands= catalogDb.GetCollection<ProductBrand>(configuration["DatabaseSettings:BrandsCollectionName"]);
+    }
+
+    public async Task<IEnumerable<ProductBrand>> GetAllBrands()
+    {
+        return await _brands.Find(_ => true).ToListAsync();
+    }
+
+    public async Task<ProductBrand> GetBrand(string id)
+    {
+        return await _brands.Find(x => x.Id == id).FirstOrDefaultAsync();
+    }
+}

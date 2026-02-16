bb0d07799742892334e79886f222ee293c6fd9d9
Vinicius Vieira <viniciusmv91@gmail.com>
2026-02-16 19:37:48 -0300

    Add IProductRepository interface for product data operations
    Introduced the IProductRepository interface in the Catalog.Core.Repositories namespace. This interface defines async methods for CRUD operations on products, as well as retrieval by name, brand, and ID. It also includes methods for paginated queries and fetching related brand and type entities.

---
+++ Services/Catalog/Catalog.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.Core.Entities;
+using Catalog.Core.Specifications;
+
+namespace Catalog.Core.Repositories;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAllProducts();
+
+    Task<Pagination<Product>> GetProducts(CatalogSpecParams specParams);
+     
+    Task<IEnumerable<Product>> GetProductsByName(string  productName);
+
+    Task<IEnumerable<Product>> GetProductsByBrand(string brandName);
+
+    Task<Product> GetProduct(string productId);
+
+    Task<Product> CreateProduct(Product product);
+
+    Task<bool> UpdateProduct(Product product);
+
+    Task<bool> DeleteProduct(string productId);
+
+    Task<ProductBrand> GetBrandsById(string brandId);
+
+    Task<ProductType> GetTypesById(string typeId);
+}

9e9ff5f0bcdaadf3b9c06e68183c73a02f5aa1f3
Vinicius Vieira <viniciusmv91@gmail.com>
2026-02-17 00:57:16 -0300

    Add query/handler to get products by name
    Introduced GetProductsByNameQuery and its handler to fetch products by name from the repository and map them to ProductResponse objects. Included necessary usings and namespace setup.

---
+++ Services/Catalog/Catalog.Application/Queries/GetProductsByNameQuery.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.Application.Mappers;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Queries;
+
+public record class GetProductsByNameQuery(string ProductName) : IRequest<IEnumerable<ProductResponse>>
+{
+}
+
+public record GetProductsByNameQueryHandler : IRequestHandler<GetProductsByNameQuery, IEnumerable<ProductResponse>>
+{
+    private readonly IProductRepository _productRepository;
+
+    public GetProductsByNameQueryHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<IEnumerable<ProductResponse>> Handle(GetProductsByNameQuery request, CancellationToken cancellationToken)
+    {
+        var products = await _productRepository.GetProductsByName(request.ProductName);
+        return products.ToResponse();
+    }
+}

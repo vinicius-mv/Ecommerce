1759bbe0860e72c64167c406856ec7cc801180b5
Vinicius Vieira <viniciusmv91@gmail.com>
2026-02-17 00:54:02 -0300

    Add GetProductByIdQuery and handler to Catalog app
    Introduced GetProductByIdQuery and its handler using MediatR to fetch a product by ID from the repository and map it to ProductResponse. Added necessary usings and namespace.

---
+++ Services/Catalog/Catalog.Application/Queries/GetProductByIdQuery.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.Application.Mappers;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Queries;
+
+public record class GetProductByIdQuery(string ProductId) : IRequest<ProductResponse>
+{
+}
+
+public record class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, ProductResponse>
+{
+    private readonly IProductRepository _productRepository;
+
+    public GetProductByIdQueryHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<ProductResponse> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+    {
+        var product = await _productRepository.GetProduct(request.ProductId);
+
+        return product.ToResponse();
+    }
+}

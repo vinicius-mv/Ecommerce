522fe3d7be27557d10f7c6ae2cfe85fede169e95
Vinicius Vieira <viniciusmv91@gmail.com>
2026-02-17 20:02:51 -0300

    Add basket and checkout entity models; remove placeholder
    Introduced BasketCheckout, ShoppingCart, and ShoppingCartItem classes under Basket.Core.Entities to model basket and checkout domain. Removed the placeholder Class1 class from Basket.Core.

---
+++ Services/Basket/Basket.Core/Entities/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.Core.Entities;
+
+public class ShoppingCart
+{
+    public string UserName { get; private set; }
+    public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+    public ShoppingCart(string userName)
+    {
+        UserName = userName;
+    }
+
+    protected ShoppingCart()    // constructor for rehydration
+    {
+    }
+}

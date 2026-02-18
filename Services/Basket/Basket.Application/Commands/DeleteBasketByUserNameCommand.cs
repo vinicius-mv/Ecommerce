fa808aa886c642ea0f9fc6797986e856c58efadc
Vinicius Vieira <viniciusmv91@gmail.com>
2026-02-17 21:28:47 -0300

    Add command to delete basket by username via MediatR
    Introduced DeleteBasketByUserNameCommand and handler using MediatR to support basket deletion by username. Handler leverages IBasketRepository for async deletion. Removed folder references for Commands and DTOs from Basket.Application.csproj.

---
+++ Services/Basket/Basket.Application/Commands/DeleteBasketByUserNameCommand.cs
@@ -0,0 +1,24 @@
+﻿using Basket.Core.Repositories;
+using MediatR;
+
+namespace Basket.Application.Commands;
+
+public record class DeleteBasketByUserNameCommand(string UserName) : IRequest<Unit>
+{
+}
+
+public record class DeleteBasketByUserNameCommandHandler : IRequestHandler<DeleteBasketByUserNameCommand, Unit>
+{
+    private readonly IBasketRepository _basketRepository;
+
+    public DeleteBasketByUserNameCommandHandler(IBasketRepository basketRepository)
+    {
+        _basketRepository = basketRepository;
+    }
+
+    public async Task<Unit> Handle(DeleteBasketByUserNameCommand request, CancellationToken cancellationToken)
+    {
+        await _basketRepository.DeleteBasket(request.UserName);
+        return Unit.Value;
+    }
+}
